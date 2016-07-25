0e113320a074759097984db921ce405bafcb0ee8
Francois Best <francoisbest@gmail.com>
2016-07-25 12:06:11 +0200

    Connected Facebook service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventComponent } from './event.component';
+import { Event } from './facebook.types';
+import { FacebookService } from './facebook.service';
 
 @Component({
     selector: 'pygre-app',
     template: `
+    <h1>Upcoming events:</h1>
     <ul>
-        <pygre-event *ngFor='let event in events' model='event'></pygre-event> 
+        <pygre-event *ngFor='let event of events' [event]='event'></pygre-event> 
     </ul>
     `,
     directives: [
         EventComponent,
+    ],
+    providers: [
+        FacebookService,
     ]
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
-    
+    events: Event[];
+
+    constructor(private facebookService: FacebookService)
+    {
+    }
+
+    ngOnInit()
+    {
+        this.loadEvents();
+    }
+
+    loadEvents()
+    {
+        this.facebookService.loadEvents().then(events => this.events = events);
+    }
+
 }
